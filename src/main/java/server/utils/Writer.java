9aba65f2f557ebc12cedfea98326a411423eac10
ChristianGeorgiev <kristian.v.georgiev@gmail.com>
2018-02-01 13:44:44 +0200

    Implemented Writer class

--- src/main/java/server/utils/Writer.java
+++ src/main/java/server/utils/Writer.java
@@ -1,4 +1,10 @@
 package server.utils;
 
-public class Writer {
+import java.io.*;
+
+public final class Writer {
+    public static void writeAllBytes(String content, OutputStream outputStream) throws IOException {
+        DataOutputStream writer = new DataOutputStream(outputStream);
+        writer.write(content.getBytes());
+    }
 }
