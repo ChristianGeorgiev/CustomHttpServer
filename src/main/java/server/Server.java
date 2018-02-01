5545a13984f042d17db0539e3f6324c345cae60b
ChristianGeorgiev <kristian.v.georgiev@gmail.com>
2018-02-01 13:46:01 +0200

    Implemented a simple test HTTP response for testing. Added some comments in the Server class to clarify request handling.

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -2,6 +2,7 @@ package server;
 
 import server.http.RequestHandler;
 import server.utils.Reader;
+import server.utils.Writer;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -11,6 +12,7 @@ import java.net.SocketTimeoutException;
 public class Server {
     private static final int SOCKET_TIMEOUT = 2000;
     private static final String SOCKET_TIMEOUT_MESSAGE = "Timeout detected";
+    private static final String HANDLED_REQUEST_MESSAGE = "Handled request on port: ";
 
 
     private int port;
@@ -32,13 +34,17 @@ public class Server {
                 clientSocket.setSoTimeout(SOCKET_TIMEOUT);
 
 
+                //====== Handling request ======
+                String requestString = Reader.readAllBytes(clientSocket.getInputStream()); //Receives the request
+                String responseString = this.handler.handleRequest(requestString); //Handles the request and creates a response
 
-                String requestString = Reader.readAllBytes(clientSocket.getInputStream());
-                this.handler.handleRequest(requestString);
+                Writer.writeAllBytes(responseString, clientSocket.getOutputStream()); //Returns the response
+                //====== Finished handling request ======
 
 
                 clientSocket.close();
-                System.out.println("Received request on port " + this.port);
+                System.out.println(HANDLED_REQUEST_MESSAGE + this.port);
+
             }catch (SocketTimeoutException e){
                 System.out.println(SOCKET_TIMEOUT_MESSAGE);
             }
--- src/main/java/server/http/RequestHandler.java
+++ src/main/java/server/http/RequestHandler.java
@@ -2,8 +2,22 @@ package server.http;
 
 public class RequestHandler {
 
-
+    // Returns the response as a String
     public String handleRequest(String requestString){
-        return null;
+        return createTestHttpResponse();
+    }
+
+
+
+    private String createTestHttpResponse(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("HTTP/1.1 200 OK").append(System.lineSeparator());
+        sb.append("Content-Type: text/html").append(System.lineSeparator());
+        sb.append(System.lineSeparator());
+        sb.append("<h1>Hello From the server!</h1>").append(System.lineSeparator());
+        sb.append("<h2>It works, obviously...</h2>");
+
+        return sb.toString();
     }
 }
